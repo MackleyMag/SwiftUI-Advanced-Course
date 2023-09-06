9d9b9f7d3da9c0c377fa3c61ef6bdeda0832d2c8
Mackley <mackleymagalhaess@hotmail.com>
2023-09-06 14:29:55 -0300

    Creating our Profile Screen

--- SwiftUI Advanced/ProfileView.swift
+++ SwiftUI Advanced/ProfileView.swift
@@ -9,7 +9,129 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile View")
+        ZStack {
+            Image("background-2")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                VStack(alignment: .leading, spacing: 16) {
+                    HStack(spacing: 16) {
+                        ZStack {
+                            Circle()
+                                .foregroundColor(Color("pink-gradient-1"))
+                                .frame(width: 66, height: 66, alignment: .center)
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.white)
+                                .font(.system(size: 24, weight: .medium, design: .rounded))
+                        }
+                        .frame(width: 66, height: 66, alignment: .center)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Sai Kambapti")
+                                .foregroundColor(.white)
+                                .font(.title2)
+                                .bold()
+                            Text("View profile")
+                                .foregroundColor(.white.opacity(0.7))
+                                .font(.footnote)
+                            
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            print("Segue to settins")
+                        } label: {
+                            TextFieldIcon(iconName: "gearshape.fill", currentEditing: .constant(true))
+                        }
+
+                    }
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.white.opacity(0.1))
+                    
+                    Text("Instructor at Design+Code")
+                        .foregroundColor(.white)
+                        .font(.title2.bold())
+                    
+                    Label("Awarded 10 certificates since September 2020", systemImage: "calendar")
+                        .foregroundColor(.white.opacity(0.7))
+                        .font(.footnote)
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.white.opacity(0.1))
+                    
+                    HStack(spacing: 16) {
+                        Image("Twitter")
+                            .resizable()
+                            .foregroundColor(.white.opacity(0.7))
+                            .frame(width: 24, height: 24, alignment: .center)
+                        
+                        Image(systemName: "link")
+                            .foregroundColor(.white.opacity(0.7))
+                            .font(.system(size: 17, weight: .semibold, design: .rounded))
+                        Text("designcode.io")
+                            .foregroundColor(.white.opacity(0.7))
+                            .font(.footnote)
+                        
+                    }
+                }
+                .padding(16)
+                
+                
+                GradientButton(buttonTitle: "Purchase Lifetime Pro Plan") {
+                    print("IAP")
+                }
+                .padding(.horizontal, 16)
+                
+                Button {
+                    print("Restore")
+                } label: {
+                    GradientText(text: "Restore Purchases")
+                        .font(.footnote.bold())
+                }
+                .padding(.bottom)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(.white.opacity(0.2))
+                    .background(Color("secondaryBackground").opacity(0.5))
+                    .background(VisualEffectBlur(blurStyle: .dark))
+                    .shadow(color: Color("shadowColor"), radius: 60, x: 0.0, y: 30.0)
+                
+            )
+            .cornerRadius(30)
+            .padding(.horizontal)
+            
+            VStack {
+                Spacer()
+                Button {
+                    print("Sign out")
+                } label: {
+                    Image(systemName: "arrow.turn.up.forward.iphone.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 15, weight: .medium, design: .rounded))
+                        .rotation3DEffect(Angle(degrees: 180), axis: (x: 0.0, y: 0.0, z: 1.0))
+                        .background(
+                            Circle()
+                                .stroke(.white.opacity(0.2), lineWidth: 1)
+                                .frame(width: 42, height: 42, alignment: .center)
+                                .overlay(
+                                    VisualEffectBlur(blurStyle: .dark)
+                                        .cornerRadius(21)
+                                        .frame(width: 42, height: 42, alignment: .center)
+                                )
+                        )
+                }
+
+            }
+            .padding(.bottom, 64)
+        }
+        .colorScheme(.dark)
     }
 }
 
